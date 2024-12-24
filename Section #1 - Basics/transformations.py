562384026177cd7ca366b04400046ea1b2df485c
vp-1990 <vinayak.pevekar@gmail.com>
2024-12-25 02:18:27 +0530

    rotate without corp code logic

--- Section #1 - Basics/transformations.py
+++ Section #1 - Basics/transformations.py
@@ -3,6 +3,10 @@
 import cv2 as cv
 import numpy as np
 
+import os
+import sys
+os.chdir(os.path.split(sys.argv[0])[0])
+
 img = cv.imread('../Resources/Photos/park.jpg')
 cv.imshow('Park', img)
 
@@ -32,8 +36,35 @@ def rotate(img, angle, rotPoint=None):
 
     return cv.warpAffine(img, rotMat, dimensions)
 
+# Rotation
+def rotate_without_corp(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+    
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotMat[0,0]) 
+    abs_sin = abs(rotMat[0,1])
+    
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+    
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotMat[0, 2] += bound_w/2 - rotPoint[0]
+    rotMat[1, 2] += bound_h/2 - rotPoint[1]
+
+    return cv.warpAffine(img, rotMat, (bound_w, bound_h))
+
 rotated = rotate(img, -45)
 cv.imshow('Rotated', rotated)
+
+rotated = rotate_without_corp(img, -45)
+cv.imshow('rotate_without_corp', rotated)
 
 rotated_rotated = rotate(img, -90)
 cv.imshow('Rotated Rotated', rotated_rotated)
